c01addb0a3afaf4d99d2b6cbc429b86769ad8116
RaheelAnsari1 <160278716+RaheelAnsari1@users.noreply.github.com>
2024-03-30 19:39:03 +0500

    tsc run

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 #! /usr/bin/env node
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const inquirer_1 = __importDefault(require("inquirer"));
-const answer = await inquirer_1.default.prompt([
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
     { message: "Enter first number", type: "number", name: "firstNumber" },
     { message: "Enter second number", type: "number", name: "secondNumber" },
     {
